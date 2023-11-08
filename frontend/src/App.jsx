d222ff955f07c69a79983c7b2876184e7698de01
Arif <arifronyahmed@gmail.com>
2023-11-08 14:19:00 +0100

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
-// import Navbar from "./components/navbar/Navbar";
+import Test from "./components/Test";
+import HomePage from "./components/homepage/HomePage";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <h1>Test</h1>
-      {/* <Navbar />
-    <HomePage/> */}
-    </>
+    <BrowserRouter>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,25 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <nav className="bg-blue-500 p-4">
-      <div className="container mx-auto flex items-center">
-        <a href="/" className="text-white text-2xl font-semibold">
+    <nav className="bg-violet-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-white text-2xl font-semibold">
           React Profile
-        </a>
+        </Link>
         <ul className="space-x-4 flex">
           <li>
-            <a href="#" className="text-white">
-              Create Profile
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white">
-              All Profile
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-white">
-              Profile Details
-            </a>
+            <Link to="/test" className="text-white">
+              test
+            </Link>
           </li>
         </ul>
       </div>
